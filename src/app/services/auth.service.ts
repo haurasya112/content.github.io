141344a0c6eef2c7936b9d647199334a7a4ac996
Haura Rasya <haura.rasya@ui.ac.id>
2023-07-30 18:28:03 +0700

    login alert

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 import { Observable, BehaviorSubject } from 'rxjs';
-import { first, catchError, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { User } from '../models/User';
 import { ErrorHandlerService } from './error-handler.service';
@@ -31,33 +31,17 @@ export class AuthService {
     private router: Router
   ) {}
 
-  login(
-    email: Pick<User, 'email'>,
-    password: Pick<User, 'password'>
-  ): Observable<{
-    token: string;
-    userId: Pick<User, 'id'>;
-  }> {
+  login(email: Pick<User, 'email'>, password: Pick<User, 'password'>): Observable<{ token: string; userId: Pick<User, 'id'> }> {
     return this.http
-      .post<{ token: string; userId: Pick<User, 'id'> }>( // Specify the expected response type here
-        `${this.url}/login`,
-        { email, password },
-        this.httpOptions
-      )
+      .post<{ token: string; userId: Pick<User, 'id'> }>(`${this.url}/login`, { email, password }, this.httpOptions)
       .pipe(
-        first(),
         tap((tokenObject) => {
           this.userId = tokenObject.userId;
           localStorage.setItem('token', tokenObject.token);
           this.isUserLoggedIn$.next(true);
           this.router.navigate(['']);
         }),
-        catchError(
-          this.errorHandlerService.handleError<{
-            token: string;
-            userId: Pick<User, 'id'>;
-          }>('login')
-        )
+        catchError(this.errorHandlerService.handleErrorAndThrow<{ token: string; userId: Pick<User, 'id'> }>('login'))
       );
   }
 }
--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from "@angular/core";
 
-import { Observable, of } from "rxjs";
+import { Observable, throwError } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class ErrorHandlerService {
-  handleError<T>(operation = "operation", result?: T) {
+  handleErrorAndThrow<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
+      if (error.status === 401) {
+        alert('Unauthorized. Please log in again.');
+      }
+      return throwError(error);
     };
   }
 }
