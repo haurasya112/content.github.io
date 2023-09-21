926849d2d57c30bb2d85a7be3fe0ca1da817cba0
Haura Rasya <haura.rasya@ui.ac.id>
2023-09-21 10:55:53 +0700

    updated

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -7,7 +8,7 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
   isAuthenticated = false;
 
   constructor(private authService: AuthService, private router: Router) {}
@@ -22,5 +23,90 @@ export class SidebarComponent implements OnInit {
     localStorage.removeItem('token');
     this.authService.isUserLoggedIn$.next(false);
     this.router.navigate(['login']);
+  }
+  
+  status = false;
+  tabs = [
+    { title: 'Tab 1', userInput: '' },
+    { title: 'Tab 2', userInput: '' },
+  ];
+  selected = new FormControl(0);
+  selectAfterAdding = true;
+  generateDisabled = [false, false];
+  showCard = [false, false];
+  modalOpen = false;
+  showScrollButton = false;
+  isProfileDropdownOpen = false;
+
+  toggleProfileDropdown() {
+    this.isProfileDropdownOpen = !this.isProfileDropdownOpen;
+  }
+
+  openModal() {
+    this.modalOpen = true;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollButton = window.scrollY > 350;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  generateClicked(index: number) {
+    if (!this.isValidInput(index)) {
+      alert('Please enter a valid input!');
+      return;
+    }
+
+    this.generateDisabled[index] = true;
+    this.showCard[index] = true;
+    this.tabs[index].title = this.tabs[index].userInput;
+    // console.log(`Generated content for Tab ${this.tabs}:`);
+  }
+
+  isValidInput(index: number): boolean {
+    if (
+      this.tabs[index].userInput === '' ||
+      this.tabs[index].userInput.trim() === ''
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  resetInputs(index: number) {
+    this.tabs[index].userInput = '';
+    this.generateDisabled[index] = false;
+    this.showCard[index] = false;
+  }
+
+  addTab(selectAfterAdding: boolean) {
+    this.tabs.push({ title: 'New', userInput: '' });
+    this.generateDisabled.push(false);
+    this.showCard.push(false);
+
+    if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+    this.generateDisabled.splice(index, 1);
+    this.showCard.splice(index, 1);
+
+    if (this.selected.value !== null && this.selected.value > index) {
+      this.selected.setValue(this.selected.value - 1);
+    }
+  }
+  addToggle() {
+    this.status = !this.status;
   }
 }
